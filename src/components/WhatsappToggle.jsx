04b45fb96b4c59cad6514f76c94f9716096875df
nadil11 <nadilnidhal111@gmail.com>
2024-07-29 18:22:29 +0530

    last commit

--- src/components/WhatsappToggle.jsx
+++ src/components/WhatsappToggle.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import { IoLogoWhatsapp } from 'react-icons/io'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const WhatsappToggle = () => {
   return (
-    <div className='absolute bottom-5 right-10 '>
-        <Link to="https://wa.me/+918330895127" target='_blank' className='btn p-0 hover:bg-transparent border-none'>
-        <IoLogoWhatsapp size={34} color='green'/>
+    <div className="relative">
+      <div className="fixed bottom-5 right-10 ">
+        <Link
+          to="https://wa.me/+918330895127"
+          target="_blank"
+          className="btn p-0 bg-transparent hover:bg-transparent border-none"
+        >
+          <IoLogoWhatsapp size={34} color="green" />
         </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default WhatsappToggle
+export default WhatsappToggle;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,18 +2,18 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar";
 import { Outlet } from "react-router-dom";
 import WhatsappToggle from "../../components/WhatsappToggle";
-import "./style.css"
+import "./style.css";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <WhatsappToggle />
       <div className="px-3 md:px-10 mt-10 mb-10 mx-auto max-w-[1440px]">
         <div className="">
           <Outlet />
         </div>
       </div>
+      <WhatsappToggle />
     </div>
   );
 };
