0ae6038ce85c0a5ddc73e38058583d33b296dbf7
nadil11 <nadilnidhal111@gmail.com>
2024-08-23 09:28:26 +0530

    last commit

--- vite.config.js
+++ vite.config.js
@@ -8,19 +8,19 @@ dotenv.config();
 export default defineConfig({
   plugins: [react()],
   server: {
-    // proxy: {
-    //     '/api': {
-    //       target: 'https://verceltest-api.vercel.app',
-    //       changeOrigin: true,
-    //       rewrite: (path) => path.replace(/^\/api/, ''),
-    //     },
-    //   },
-      proxy: {
+    proxy: {
         '/api': {
-          target: 'http://localhost:5000',
+          target: 'https://verceltest-api.vercel.app',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
       },
+      // proxy: {
+      //   '/api': {
+      //     target: 'http://localhost:5000',
+      //     changeOrigin: true,
+      //     rewrite: (path) => path.replace(/^\/api/, ''),
+      //   },
+      // },
   },
 });
